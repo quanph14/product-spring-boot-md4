970f329670814b3a01d6460be9199c3788885cbb
quanph14 <phquan.0493@gmail.com>
2022-11-04 15:25:38 +0700

    product-manager-springboot

---
+++ src/main/java/com/codegym/productspringboot/service/product/IProductService.java
@@ -0,0 +1,7 @@
+package com.codegym.productspringboot.service.product;
+
+import com.codegym.productspringboot.model.Product;
+import com.codegym.productspringboot.service.IGeneralService;
+
+public interface IProductService extends IGeneralService<Product> {
+}
